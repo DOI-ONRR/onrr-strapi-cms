4cead8c421b534a60be827c3c02fc760cc33762e
Jerome <jerome.louis@onrr.gov>
2020-12-01 13:07:49 -0700

    server config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', process.env.HOST || '0.0.0.0'),
+  port: env.int('PORT', process.env.PORT || 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'c2cfd439aed4d7bf9c4b5a83e358808f'),
